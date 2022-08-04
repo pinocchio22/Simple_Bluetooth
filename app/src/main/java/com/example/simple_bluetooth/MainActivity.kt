eae34a44f7d341aa4669ade9e9741fd6d94693f7
pinocchio22 <vviian.2@gmail.com>
2022-08-04 18:13:06 +0900

    check permission

--- app/src/main/java/com/example/simple_bluetooth/MainActivity.kt
+++ app/src/main/java/com/example/simple_bluetooth/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.simple_bluetooth
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.simple_bluetooth.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
 
     private val REQUEST_ENABLE_BT = 1
+    private val REQUEST_PERMISSIONS = 2
+    private val PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
     private var bluetoothAdapter: BluetoothAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +32,32 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    fun hasPermissions(context: Context?, permissions: Array<String>) : Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (permission in permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+                REQUEST_PERMISSIONS -> {
+                    if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "Permissions granted!", Toast.LENGTH_LONG).show()
+                    } else {
+                        requestPermissions(permissions, REQUEST_PERMISSIONS)
+                        Toast.makeText(this, "Permissions must be granted", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
 
     @SuppressLint("MissingPermission")
     fun bluetoothcheck() {
